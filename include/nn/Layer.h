b31e65f9af65475f8c1e2df996b20d2e96c94e41
Emon-36 <emon23702036@gmail.com>
2026-03-10 17:13:05 +0600

    Neural Layer

--- include/nn/Layer.h
+++ include/nn/Layer.h
@@ -13,7 +13,27 @@ namespace Emo {
     public:
         Matrix weights;
         Matrix bias;
-        DenseLayer(int in_size, int out_size, std::string act="relu");
+        DenseLayer(int in_size, int out_size, std::string act="relu")
+        :weights(out_size, in_size), bias(out_size, 1), activation(act) {
+            this->weights.fillRandom(-1.0f, 1.0f);
+            this->bias.fillRandom(-1.0f, 1.0f);
+        }
+
+        Matrix forward(const Matrix& input) {
+            Matrix output = this->weights.dot(input);
+            output.addBias(this->bias);
+            if (activation == "relu") {
+                output.relu();
+            } else if (activation == "sigmoid") {
+                output.sigmoid();
+            } else if (activation == "sigmoid") {
+                output.sigmoid();
+            }
+
+            return output;
+        }
+
+        
        
     };
     
